6ffdc39ddd7b73e5efd1238fc3c3dedc356df4e7
Pinaka07 <anandsingh.as1996@gmail.com>
2026-02-10 18:42:51 +0530

    42

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -54,6 +54,9 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()        
+    train_data,test_data=obj.initiate_data_ingestion()
     data_transformation=DataTransformation()
-    train_array,test_array=data_transformation.initiate_data_transformation(train_data,test_data)
+    train_array,test_array,preprocessor_path=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_array,test_array,preprocessor_path))
